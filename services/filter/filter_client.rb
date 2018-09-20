a2f0ad8c803584ff8abe44cd9a9f14233f0aa232
Eiji Yamamoto <yamamoto-e@swlab.cs.okayama-u.ac.jp>
2018-09-20 15:51:22 +0900

    Use google/protobuf/well_known_types to convert Time class into DateOrTime

--- services/filter/filter_client.rb
+++ services/filter/filter_client.rb
@@ -7,22 +7,24 @@ require 'grpc'
 require "hiyoco/calendar/event_pb"
 require "hiyoco/filter/service_pb"
 require "hiyoco/filter/service_services_pb"
-require 'date'
+require "google/protobuf/well_known_types"
 
 def create_date(time)
-    gtime = Google::Protobuf::Timestamp.new
-    gtime.from_time(time)
-    d = Hiyoco::Calendar::Date.new(date: gtime)
-    return d
+  gtime = Google::Protobuf::Timestamp.new
+  gtime.from_time(time)
+  d = Hiyoco::Calendar::Date.new(date: gtime)
+  date_or_time = Hiyoco::Calendar::DateOrTime.new
+  date_or_time.date = d
+  return date_or_time
 end
 
 def main
-  start_time = DateTime.parse("2018-08-06T13:00:00+09:00")
-  end_time = DateTime.parse("2018-08-06T15:00:00+09:00")
+  start_time = Time.new(2018, 8, 6, 13, 30, 0, "+09:00")
+  end_time = Time.new(2018, 8, 6, 15, 30, 0, "+09:00")
   summary = "第160回GN談話会"
-  description = nil
+  description = ""
 
-  stub = Hiyoco::Filter::Filter::Stub.new('0.0.0.0:50051', :this_channel_is_insecure)
+  stub = Hiyoco::Filter::Filter::Stub.new('0.0.0.0:50050', :this_channel_is_insecure)
   ev = Hiyoco::Calendar::Event.new(start: create_date(start_time), end: create_date(end_time), summary: summary, description: description)
   message = stub.say_event(ev).result
   puts message
