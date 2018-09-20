140721a32b9e27813dcbad871b67b440c6393249
Eiji Yamamoto <yamamoto-e@swlab.cs.okayama-u.ac.jp>
2018-09-20 15:43:09 +0900

    Modify compile_proto.sh to use informant stub

--- services/filter/compile_proto.sh
+++ services/filter/compile_proto.sh
@@ -4,3 +4,4 @@ SH_PATH=$(dirname $0)
 
 bundle exec grpc_tools_ruby_protoc -I ../../proto/ --ruby_out=lib/proto --grpc_out=lib/proto ../../proto/hiyoco/calendar/event.proto
 bundle exec grpc_tools_ruby_protoc -I ../../proto/ --ruby_out=lib/proto --grpc_out=lib/proto ../../proto/hiyoco/filter/service.proto
+bundle exec grpc_tools_ruby_protoc -I ../../proto/ --ruby_out=lib/proto --grpc_out=lib/proto ../../proto/hiyoco/informant/service.proto
