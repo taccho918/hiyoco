36f3b09687579b742369cff35deb02360ad9472d
Eiji Yamamoto <yamamoto-e@swlab.cs.okayama-u.ac.jp>
2018-09-20 15:52:13 +0900

    Fix filter service to connect informant

--- services/filter/filter_server.rb
+++ services/filter/filter_server.rb
@@ -6,22 +6,33 @@ require 'grpc'
 require "hiyoco/calendar/event_pb"
 require "hiyoco/filter/service_pb"
 require "hiyoco/filter/service_services_pb"
-
-host = "localhost"
-port = 50051
+require "hiyoco/informant/service_services_pb"
 
 class FilterServer < Hiyoco::Filter::Filter::Service
   def say_event(e, _unused_call)
-    stub = Hiyoco::Filter::Filter::Stub.new('0.0.0.0:50050', :this_channel_is_insecure)
-    str = "Summary:#{e.summary}\nDescription:#{e.description}\nStart at #{e.start_time.strftime("%Y-%m-%d %H:%M")}\nEnd at #{e.end_time.strftime("%Y-%m-%d %H:%M")}\n"
+    stub = Hiyoco::Informant::Informant::Stub.new('0.0.0.0:50051', :this_channel_is_insecure)
+    start_time = Time.at(e.start.date.date.seconds)
+    end_time = Time.at(e.end.date.date.seconds)
+    str = "Summary:#{e.summary}\nDescription:#{e.description}\nStart at #{start_time.strftime("%Y-%m-%d %H:%M")}\nEnd at #{end_time.strftime("%Y-%m-%d %H:%M")}\n"
     message = stub.say_event(Hiyoco::Calendar::Text.new(body: str )).result
     puts message
   end
 end
 
 def main
+  if ARGV[0] == nil then
+    host = "localhost"
+  else
+    host = ARGV[0]
+  end
+  if ARGV[1] == nil then
+    port = 50050
+  else
+    port = ARGV[1]
+  end
+
   s = GRPC::RpcServer.new
-  s.add_http2_port('0.0.0.0:50051', :this_port_is_insecure)
+  s.add_http2_port("#{host}:#{port}", :this_port_is_insecure)
   s.handle(FilterServer)
   s.run_till_terminated
 end
